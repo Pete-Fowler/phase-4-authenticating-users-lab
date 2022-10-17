b53b716055780e9d7756cd6fd2da7bf10645980c
Pete Fowler <fowler.pete@gmail.com>
2022-10-17 12:34:12 -0600

    Create sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
-  post '/login', to: 'SessionsController#create'
-  delete '/logout', to: 'SessionsController#destroy'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
